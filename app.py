11829242dc7e042fe9d1135042688816196b648a
Amit Dharmadhikari <amitdharmadhikari41@gmail.com>
2019-03-12 23:42:35 -0400

    Ready to submit.

--- app.py
+++ app.py
@@ -38,8 +38,7 @@ def index():
     pca_random_sampling_results_top_3_attributes_names = df.columns[pca_random_sampling_results_top_3_attributes]
     pca_stratified_sampling_results_top_3_attributes_names = df.columns[
         pca_stratified_sampling_results_top_3_attributes]
-    # print("top 3")
-    # print(pca_no_sampling_results_top_3_attributes)
+
 
     pca_no_sampling_projected_points = np.dot(stratified_sampling_results, pca_no_sampling_top_two_components.T)
     pca_random_sampling_projected_points = np.dot(stratified_sampling_results, pca_random_sampling_top_two_components.T)
@@ -58,21 +57,7 @@ def index():
     dissimilarity_matrix = np.nan_to_num(dissimilarity_matrix)
     mds_corr.fit(dissimilarity_matrix)
     mds_corr_points = np.array(mds_corr.embedding_ * 10, dtype=int)
-    print(mds_corr_points.shape)
-    print(mds_corr_points)
-    # print(np.sum(np.isnan(dissimilarity_matrix)))
-    # print(np.sum(np.isnan(df_dimension_reduced)))
 
-    # print(mds.embedding_.shape)
-    # print(mds.embedding_)
-
-    # print(dissimilarity_matrix)
-
-    # print(len(random_sampling_results))
-    # print(len(stratified_sampling_results))
-    # print(np.cumsum(pca_no_sampling_results))
-    # print(np.cumsum(pca_random_sampling_results))
-    # print(np.cumsum(pca_stratified_sampling_results))
 
     data_frontend = dict()
     data_frontend["pca_no_sampling_variance_ratios"] = json.dumps(pca_no_sampling_results.tolist())
@@ -93,15 +78,8 @@ def index():
     data_frontend["pca_stratified_sampling_results_top_3_attributes_names"] = json.dumps(
         pca_stratified_sampling_results_top_3_attributes_names.tolist())
 
-    # data_frontend["pca_no_sampling_scree_plot_data"] = json.dumps([json.dumps(
-    #     {"factor": i + 1, "eigenvalue": pca_no_sampling_results[i],
-    #      "cumulative_eigenvalue": np.cumsum(pca_no_sampling_results)[i]}) for i in range(19)])
-    # data_frontend["pca_random_sampling_scree_plot_data"] = json.dumps([json.dumps(
-    #     {"factor": i + 1, "eigenvalue": pca_random_sampling_results[i],
-    #      "cumulative_eigenvalue": np.cumsum(pca_random_sampling_results)[i]}) for i in range(19)])
-    # data_frontend["pca_stratified_sampling_scree_plot_data"] = json.dumps([json.dumps(
-    #     {"factor": i + 1, "eigenvalue": pca_stratified_sampling_results[i],
-    #      "cumulative_eigenvalue": np.cumsum(pca_stratified_sampling_results)[i]}) for i in range(19)])
+    print(pca_stratified_sampling_results_top_3_attributes_names)
+
 
     data_frontend["pca_no_sampling_scree_plot_data"] = [{"factor": i + 1, "eigenvalue": pca_no_sampling_results[i],
          "cumulative_eigenvalue": np.cumsum(pca_no_sampling_results)[i]} for i in range(19)]
@@ -120,7 +98,6 @@ def index():
     data_frontend["mds_corr_data"] = [{"x": i[0], "y": i[1]} for i in mds_corr_points]
     print(data_frontend["scatterplot_matrix_data"])
 
-    # print(json.dumps(json.loads(data_frontend["pca_no_sampling_projected_points"]), indent=4))
 
     data_frontend = {'chart_data': data_frontend}
 
@@ -129,44 +106,35 @@ def index():
 
 def random_sampling(data):
     data_arr = np.array(data)
-    sample_size = int(0.25 * len(data_arr))
+    sample_size = int(0.05 * len(data_arr))
     random_indices = random.sample(range(len(data_arr)), sample_size)
     random_sampling_results = data_arr[random_indices]
-    # print(len(data_arr))
-    # print(len(random_sampling_results))
     return random_sampling_results
 
 
 def stratified_sampling(data):
     data_arr = np.array(data)
-    # print(len(data))
     sse_array = []
     for i in range(2, 20):
         km = KMeans(n_clusters=i)
         km.fit(data_arr)
         sse_array.append(km.inertia_)
-    # print(sse_array)
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.plot(range(2, 20), sse_array, marker='o', markeredgecolor='r', color='b')
-    # # ax.plot(range(2, 20), sse_array)
-    # plt.grid(True)
-    # plt.xlabel('Number of clusters')
-    # plt.ylabel('Sum of Squared Errors (SSE)')
-    # plt.title('Elbow plot for K-Means clustering')
-    # plt.xticks(range(2, 20, 2))
-    # # print("End of plotElbow")
-    # plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(range(2, 20), sse_array, marker='o', markeredgecolor='r', color='b')
+    # ax.plot(range(2, 20), sse_array)
+    plt.grid(True)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Sum of Squared Errors (SSE)')
+    plt.title('Elbow plot for K-Means clustering')
+    plt.xticks(range(2, 20, 2))
+    plt.show()
 
     km = KMeans(n_clusters=6)
     km.fit(data_arr)
     data['Label'] = km.labels_
-    # print(df['Label'])  #Gives 18206 entries for some reason
-    # print(len(df))
-    # print(len(df['Label']))
     cluster_sizes = np.bincount(km.labels_)
-    # stratified_sampling_results.columns = df.columns
     stratified_sampling_results = pd.DataFrame(columns=data.columns)
     for i in range(6):
         cluster_size = cluster_sizes[i]
@@ -174,9 +142,6 @@ def stratified_sampling(data):
         sample_size = int(cluster_size * 0.05)
         stratified_sampling_results = pd.concat(
             [stratified_sampling_results, cluster_records.iloc[random.sample(range(cluster_size), sample_size)]])
-    # print("Stratified sampling results length: " + str(len(stratified_sampling_results)))
-    # print("Sum of sizes" + str(sum(cluster_sizes)))
-    # print(stratified_sampling_results)
     return sse_array, stratified_sampling_results
 
 
